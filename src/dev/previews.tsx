777da361f77d177881d49af739b79ce80e769023
Alan <alanxiao44@gmail.com>
2023-11-19 13:00:01 -0500

    Add dev components

---
+++ src/dev/previews.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
+import {PaletteTree} from "./palette";
+import {AlcorForms} from "../components/alcor-forms/AlcorForms";
+import {Gallery} from "../components/gallery/Gallery";
+import App from "../App";
+import {Main} from "../components/Main";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/AlcorForms">
+                <AlcorForms/>
+            </ComponentPreview>
+            <ComponentPreview path="/Gallery">
+                <Gallery/>
+            </ComponentPreview>
+            <ComponentPreview path="/App">
+                <App/>
+            </ComponentPreview>
+            <ComponentPreview path="/Main">
+                <Main/>
+            </ComponentPreview>
+            <ComponentPreview path="/ComponentPreviews">
+                <ComponentPreviews/>
+            </ComponentPreview>
+        </Previews>
+    );
+};
+
+export default ComponentPreviews;
