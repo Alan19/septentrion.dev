a146d3fc6ae2a8130173b93cda38642d64b06142
Alan <alanxiao44@gmail.com>
2023-11-19 19:46:58 -0500

    Add images to forms page

--- src/components/alcor-forms/AlcorForms.tsx
+++ src/components/alcor-forms/AlcorForms.tsx
@@ -1,6 +1,8 @@
-import {Container, Divider, Stack, Typography} from "@mui/material";
+import {Container, Divider, Grid, Stack, Typography, useMediaQuery} from "@mui/material";
 import React from "react";
 import {Link, Outlet} from "react-router-dom";
+import './form-page.css'
+import {theme} from "../../App";
 
 export function CharacterAttribute(props: { fieldName: String, fieldValue: String }) {
     return <div>
@@ -10,28 +12,52 @@ export function CharacterAttribute(props: { fieldName: String, fieldValue: Strin
 }
 
 export function AlcorForms() {
+    const isMediumOrUp = useMediaQuery(theme.breakpoints.up('md'));
+    const formInfo: {name: string, src: string, link: string}[] = [
+        {
+            name: "Aquarius Form",
+            src: "https://alcorsiteartbucket.s3.amazonaws.com/aquarius_form_combar_outfit.png",
+            link: "aquarius"
+        },
+        {
+            name: "Jupiter Form",
+            src: "https://alcorsiteartbucket.s3.amazonaws.com/future_spark.png",
+            link: "jupiter"
+        },
+        {
+            name:"AICore Form",
+            src: "https://pbs.twimg.com/media/FcNTqa8aUAE4uED?format=jpg&name=4096x4096",
+            link: "aicore"
+        },
+        {
+            name: "M⬡ Form",
+            src: "https://alcorsiteartbucket.s3.amazonaws.com/m_form_centered.png",
+            link: "m"
+        },
+        {
+            name: "Triangulum Form",
+            src: "https://alcorsiteartbucket.s3.amazonaws.com/triangulum_form.jpg",
+            link: "triangulum"
+        }
+    ]
     return (
         <Container style={{marginTop: '8px'}}>
             <Typography variant={"h3"} fontFamily={"Origin Tech"}>Alcor's Forms</Typography>
-            {/*<AquariusForm/>*/}
-            {/*<Divider style={{marginTop: '8px', marginBottom: '8px'}}/>*/}
-            {/*<JupiterForm/>*/}
-            {/*<Divider style={{marginTop: '8px', marginBottom: '8px'}}/>*/}
-            {/*<AICoreForm/>*/}
-            {/*<Divider style={{marginTop: '8px', marginBottom: '8px'}}/>*/}
-            {/*<MForm/>*/}
-            {/*<Divider style={{marginTop: '8px', marginBottom: '8px'}}/>*/}
-            {/*<TriangulumForm/>*/}
-            <Stack direction={"row"} spacing={1}>
-                <Link to={"aquarius"}>Aquarius Form</Link>
-                <Link to={"jupiter"}>Jupiter Form</Link>
-                <Link to={"aicore"}>AICore Form</Link>
-                <Link to={"m"}>M Form</Link>
-                <Link to={"triangulum"}>Triangulum Form</Link>
-            {/*    TODO Add link to gallery*/}
-            </Stack>
-            <Divider style={{marginBottom: '32px'}}/>
-            <Outlet />
+            <Grid container direction={"row"} spacing={1}>
+                {
+                    formInfo.map(value => <Grid item md={2}>
+                        <Link to={value.link}>
+                            <figure style={{margin: 0}}>
+                                <img alt={value.name} className={'form-splash-image'} src={value.src}/>
+                                <figcaption>{value.name}</figcaption>
+                            </figure>
+                        </Link>
+                    </Grid>)
+                }
+                {/*    TODO Add link to gallery*/}
+            </Grid>
+            <Divider style={{marginBottom: '32px', marginTop: '32px'}}/>
+            <Outlet/>
         </Container>
     );
 }
