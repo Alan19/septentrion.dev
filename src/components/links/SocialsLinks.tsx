f08deb65d54d5ae9e709c9a5e55e5ed611393cb6
Alan <alanxiao44@gmail.com>
2024-08-07 00:33:45 -0400

    Fix filtered images link

--- src/components/links/SocialsLinks.tsx
+++ src/components/links/SocialsLinks.tsx
@@ -32,7 +32,7 @@ export function SocialsLinks() {
             />
 
             <Stack marginTop={2} spacing={1}>
-                <InternalLinkButton icon={<CollectionsIcon/>} text={"Gallery"} link={'/gallery?filters={"Featured"%3A"1"}'}/>
+                <InternalLinkButton icon={<CollectionsIcon/>} text={"Gallery"} link={'/gallery?filters=Featured'}/>
                 <InternalLinkButton icon={<FontAwesomeIcon icon={faDragon}/>} text={"About"} link={'/about'}/>
             </Stack>
         </>
