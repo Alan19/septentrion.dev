b7213a923cec6c7343d177100ef7087e3df1d7fa
Alan <alanxiao44@gmail.com>
2024-10-23 01:27:42 -0400

    Add Castor info page and clean up world info

--- src/components/lore/characters/templated-lore-page-info.ts
+++ src/components/lore/characters/templated-lore-page-info.ts
@@ -11,7 +11,8 @@ import jupiterIcon from "../assets/icons/jupiter-icon.webp"
 import triangulumIcon from "../assets/icons/triangulum-icon.webp"
 import biogenesisIcon from "../assets/icons/biogenesis-icon.webp"
 import alcorIcon from '../assets/icons/alcor_icon.png'
-
+import castorIcon from '../assets/icons/castor_icon.png'
+import castorSplashImage from '../assets/splash/castor-splash.png'
 
 export const templatedLorePageInfo: FormInformation[] = [
     {
@@ -407,5 +408,21 @@ export const originalCharacters: FormInformation[] = [
         imageAspectRatio: 0.6218760142810775,
         body: "Alcor is a cougar-dragon hybrid with telekinetic powers. His works as an inventor as his day job, but also enjoys streaming video games, archery, and coding.",
         history: "Alcor lives in [REDACTED] City, a "
+    },
+    {
+        name: "Castor",
+        thumbnail: castorIcon,
+        link: "castor",
+        description: "Alcor's symbiote companion, who helps keep him emotionally stable and also powers him up.",
+        image: castorSplashImage,
+        imageAspectRatio: 1,
+        body: "Castor is a symbiote that Alcor made by extracting alien DNA from a meteorite, which was then fused with his own DNA to increase the symbiote's compatibility with his own body. While he looks a bit scary and spooky, he's actually really helpful, and helps Alcor destress and unwind from work. When Alcor is incapacitated, Castor is able to seamlessly assume control of Alcor's body to defend him from further harm. This increases the percentage of exotic matter in Alcor's body, allowing Castor to mold Alcor's body into an evolved form personalized for Castor, dubbed Sentinel Mode. In Sentinel Mode, Castor tries his best to shield Alcor from further harm and heal his injuries as quickly as possible by focusing on crowd control, speed, and perception. Castor can also assume this form for short periods of time outside of combat, but with severly dimished combat abilites, so it is purely cosmetic.",
+        history: "Castor's body is made up of exotic matter, which powers a large chunk of Alcor's inventions with space warping and interfacing abilities. However, the generation rate of exotic matter is slow, so there is a long waitlist for commissions that use said material. On top of that, Alcor has his own personal projects he is working on, which makes the waitlist even longer.",
+        specs: [
+            {content: 'http://localhost:3000/static/media/castor-evolved.48729090d8e87dff4359.webp', md: 3, xs: 12},
+            {
+                content: [1, 3, 5, 5, 4, 3]
+            }
+        ]
     }
 ]
--- src/components/lore/world/AlcorWorldInfo.tsx
+++ src/components/lore/world/AlcorWorldInfo.tsx
@@ -1,16 +1,11 @@
-import {Fade, Grid, Stack, Typography} from "@mui/material";
+import {Grid, Stack, Typography} from "@mui/material";
 import React, {memo} from "react";
-import castor from "../assets/castor.webp"
-import castorEvolution from '../assets/castor-evolved.webp'
 import {AboutPageParagraph} from "../AboutPageParagraph";
 import {PageHeader} from "../PageHeader";
-import {SkeletonImage} from "../../SkeletonImage";
-import {Divider} from "@mui/material-next";
 import {croppedImageWithCurvedBorder} from "../characters/TemplatedLorePage";
 
 export const AlcorWorldInfo = memo(function AlcorWorldInfo() {
-    return <Fade in>
-        <div>
+    return <div>
             <PageHeader title={"Alcor's World"}/>
             <Grid container spacing={2}>
                 <Grid item xs={0} md={2}>
@@ -21,31 +16,20 @@ export const AlcorWorldInfo = memo(function AlcorWorldInfo() {
                         <Typography variant={"body1"}>
                             Alcor is a coguar-dragon hybrid with telekinetic powers who lives in [REDACTED] City in the year 2601. At the dawn of the 27th century, climate change, geopolitical conflicts, and resource scarcity is no longer a
                             problem. Most settlements offer high-tech amenities and ample greenspace, allowing everyone to find a place that fits their preferences. Methods of self-expression is readily available, with custom clothing and
-                            body
-                            modifications being a quick 3D-print or consultation away. While most places are peaceful and (relatively) uneventful places to live in, [REDACTED] City is filled with 'thrilling' experiences for people seeking a
-                            less
-                            peaceful lifestyle. The city's harbor and surrounding grasslands experience occasional kaiju attacks, making it a tempting destination for people who have talents in combat or engineering. The city is also has a
-                            layout
-                            and culture that helps train the parkour and fighting skills of the people who fight the kaiju, so there's plenty to do even when it's peaceful.
+                            body modifications being a quick 3D-print or consultation away. While most places are peaceful and (relatively) uneventful places to live in, [REDACTED] City is filled with 'thrilling' experiences for people
+                            seeking a more exciting and dynamic lifestyle.
+                        </Typography>
+                        <Typography variant={"body1"}>
+                            The city's harbor and surrounding grasslands experience occasional kaiju attacks, making it a tempting destination for people who have talents in combat or engineering. The city is designed for parkour as most
+                            outdoor areas are accessible for the public, with many semi-hidden rooftop plazas being hangout spots for friend groups. A mix of aerial taxi services combined with personal gravity dampeners are used by people
+                            who can't physically reach those areas, allowing them to access those hard to reach spots. There's also plenty of training events, which allows people to stay active to prepare for kaiju attacks and to improve
+                            their overall fitness.
                         </Typography>
                         <AboutPageParagraph title={"Alcor's Life"}
                                             text={"Alcor plies his trade as an inventor, creating high tech gadgets and clothing for people interested in participating in physically active activities like defense, parkour, or brawling. He also participates in parkour in his spare time, and also moonlights as a part-time superhero who rescues people caught in the middle of kaiju attacks. Alcor tries to maintain a cool and powerful image, but his friends know that he's much more geeky and kind than he looks. However, they worry too much about his self-experimentation in the pursuit of becoming stronger, as he wants to make sure he's always prepared for any fights that break out."}/>
-                        <Divider/>
-                        <AboutPageParagraph title={"Castor"}
-                                            text={"Castor is a symbiote that Alcor made by extracting alien DNA from a meteorite, which was then fused with his own DNA to increase the symbiote's compatibility with his own body. While he looks a bit scary and spooky, he's actually really helpful, and helps Alcor destress and unwind from work. Castor's body is made up of exotic matter, which powers a large chunk of Alcor's inventions with space warping and interfacing abilities. However, the generation rate of exotic matter is slow, so there is a long waitlist for commissions that use said material. On top of that, Alcor has his own personal projects he is working on, which makes the waitlist even longer."}
-                                            img={<SkeletonImage src={castor} style={croppedImageWithCurvedBorder} aspectRatio={1615 / 2240}/>}
-                                            colors={["#40A5B6", "#49FFE9", "#44D8CE", "#2D5C98"]}
-                        />
-                        <AboutPageParagraph title={"Castor β - Sentinel Mode"}
-                                            text="When Alcor is incapacitated, Castor is able to seamlessly assume control of Alcor's body to defend him from further harm. This increases the percentage of exotic matter in Alcor's body, allowing Castor to mold Alcor's body into an evolved form personalized for Castor, dubbed Sentinel Mode. In Sentinel Mode, Castor tries his best to shield Alcor from further harm and heal his injuries as quickly as possible by focusing on crowd control, speed, and perception. Castor can also assume this form for short periods of time outside of combat, but with severly dimished combat abilites, so it is purely cosmetic."
-                                            img={<SkeletonImage src={castorEvolution} style={croppedImageWithCurvedBorder} aspectRatio={1430 / 2272}/>}
-                                            variant={"h6"}
-                        />
-                        {/*    TODO Move Castor stuff to his own webpage, and add world background images*/}
                     </Stack>
-
+                    {/*TODO Add background images*/}
                 </Grid>
             </Grid>
-        </div>
-    </Fade>;
+    </div>;
 });
