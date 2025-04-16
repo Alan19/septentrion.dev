ad7eb0823147d78bff6b27fbbea41a52c2c74340
Alan <alanxiao44@gmail.com>
2025-04-16 00:54:37 -0400

    Revert domain, switch back to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import {App} from './App.tsx'
 import {CssBaseline} from '@mui/material'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <CssBaseline/>
-        <BrowserRouter basename={"/"}>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
 )
