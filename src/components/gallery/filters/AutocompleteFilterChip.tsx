dde36eb08af27c7d3fb76a6071bb8d329af0c937
Alan Xiao <alanxiao44@gmail.com>
2025-08-31 22:35:08 -0400

    Add more art

--- src/components/gallery/filters/AutocompleteFilterChip.tsx
+++ src/components/gallery/filters/AutocompleteFilterChip.tsx
@@ -1,6 +1,6 @@
 import Chip from "@mui/material-next/Chip";
 
-export function AutocompleteFilterChip(props: Readonly<{ option: string, tagProps: { key: number; className: string; disabled: boolean; "data-tag-index": number; tabIndex: -1; onDelete: (event: any) => void } }>) {
+export function AutocompleteFilterChip(props: Readonly<{ option: string, tagProps: { key: number; className: string; disabled: boolean; "data-tag-index": number; tabIndex: -1; onDelete: (event: never) => void } }>) {
     return <Chip
         color={props.option.startsWith("-") ? "error" : "primary"}
         {...props.tagProps}
